80dd66d95bd0f5d418eedc27dda19f683351692f
agungTuanany <agung.tuanany@gmail.com>
2020-05-02 09:50:40 +0700

    Chapter_2 Enhancing Node Performance | Web Worker Threads

--- example/index.js
+++ example/index.js
@@ -1,48 +1,47 @@
 "use strict"
 /*
  * Server with express | I don't like to use 3rd party libs when in cours
- * Implement "cluster mode"
  *
- * test the cluster with:
- * ? ab -c 6 -n 6 localhost:8000/
+ * XXX TODO use NodeJS standart lib for "worker_threads" either installing
+ * "webworker-threads" in NodeJS V12-LTS.XXX
+ * XXX FIXME: This code need to change!!! cause not working XXX
  *
  */
 
-// Init Threadpool
-process.env.UV_THREADPOOL_SIZE = 1;
-
 // Dependencies
-const cluster		= require ("cluster")
+const crypto		= require ("crypto")
+const express		= require ('express')
+const app			= express ()
+const Worker		= require ("worker_threads")
 
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-	// Cause index.js to be executed *again* but in slave (child) mode
-	cluster.fork ()
-	cluster.fork ()
-	console.log ("cluster isMaster when called fork () ",cluster.isMaster)
-}
-else {
-	// I'm a child, I'm going to act like a server and do nothing else
-	const crypto		= require ("crypto")
-	const express		= require ('express')
-	const app			= express ()
+const port = 8000
 
-	const port = 8000
+app.get ("/", (req, res) => {
+	const worker = new Worker ( function () {
+		this.onmessage = function () {
+			let counter = 0
+			while (conter < 1e9) {
+				counter++
+			}
 
-	app.get ("/", (req, res) => {
-		crypto.pbkdf2 ("a", "b", 100000, 512, "sha512", () => {
-			res.send ("Hi There")
-		})
+
+			postMessage (counter)
+		}
 	})
 
-	app.get ("/fast", (req, res) => {
-		res.send ("This was fast")
-	})
+	worker.onmessage = function (message) {
+		console.log (message.data)
+		res.send (""+message.data)
+	}
 
-	app.listen (port, () => {
-		console.log ('\x1b[36m%s\x1b[0m',"index.js is running on port:", port)
-	})
-	console.log ("cluster isMaster: ",cluster.isMaster)
-}
+	worker.postMessage ()
+})
 
+app.get ("/fast", (req, res) => {
+	res.send ("This was fast")
+})
+
+app.listen (port, () => {
+	console.log ('\x1b[36m%s\x1b[0m',"index.js is running on port:", port)
+})
 
